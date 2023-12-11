bf2ef08bfee5f8e794ea2b79144ced251e09476c
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-12-11 18:04:17 -0500

    react-hooks-lecture-videos-link-and-navlink/
    react-hooks-lecture-videos-link-and-navlink/

---
+++ react-hooks-lecture-videos-link-and-navlink-main/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div id="home">
+            <h1 style={{ color: "firebrick" }}>
+                Valencia Walker is a Web Developer from Michigan
+            </h1>
+        </div>
+    );
+}
+export default Home;
